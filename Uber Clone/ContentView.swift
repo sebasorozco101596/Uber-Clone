d78f06556a9912a3f966587be0abd75345552ad9
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-09-27 18:12:06 -0400

    Initial Commit

---
+++ Uber Clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Uber Clone
+//
+//  Created by Juan Sebastian Orozco Buitrago on 9/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Uber Clone/Uber_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uber_CloneApp.swift
+//  Uber Clone
+//
+//  Created by Juan Sebastian Orozco Buitrago on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Uber_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
